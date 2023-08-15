9c9e382787b174bfe23fe2156ca46f73ad4d610c
Radim Pavlicek <radim.pavlicek@gmail.com>
2023-08-15 14:52:38 +0200

    cognito config update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_G63AMzUWs', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4sgujg7htmm9t4gs6ik6i4ou6l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
